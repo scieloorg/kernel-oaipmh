abf5818bf2f329d5b28efcfd5df98c0408f67e5b
Gustavo Fonseca <gustavo@gfonseca.net>
2020-04-09 15:07:08 -0300

    Adiciona listSets lendo da base de dados local

--- oaipmhserver/server.py
+++ oaipmhserver/server.py
@@ -4,6 +4,9 @@ from oaipmh import common
 
 
 class OAIServer:
+    def __init__(self, session):
+        self.session = session
+
     def identify(self):
         return common.Identify(
             repositoryName='Fake',
@@ -16,10 +19,17 @@ class OAIServer:
             compression=['identity'],
         )
 
+    def listSets(self):
+        return [(s["set_spec"], s["set_name"], "") 
+                for s in self.session.documents.sets()]
+
 
 if __name__ == "__main__":
+    from oaipmhserver.adapters import mongodb
     from oaipmh import server
 
-    s = server.Server(OAIServer())
-    print(s.identify())
+    db = mongodb.MongoDB("mongodb://localhost:27017")
+    session = mongodb.Session(db)
 
+    s = server.Server(OAIServer(session), resumption_batch_size=10)
+    print(s.handleRequest({"verb": "ListSets"}))
